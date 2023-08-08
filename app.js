7cd2b86dd881ec4e024805aae5e7fa916c408da4
Ben Harrison <benharrison9898@gmail.com>
2023-08-08 23:51:07 +0100

    Added content

---
+++ app.js
@@ -0,0 +1,18 @@
+  // Set the date since which you want to count days (format: year, month - 1, day)
+  const startDate = new Date(2023, 7, 5); // January 1, 2023
+
+  function updateDaysSince() {
+    const now = new Date();
+    const timeDifference = now - startDate;
+    const daysSince = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+
+    document.getElementById("daysSince").textContent = `${daysSince} days`;
+  }
+
+  updateDaysSince();
+
+
+  var song = new Audio('song.mp3')
+  document.body.addEventListener('click', function() { 
+    //song.play()
+   });
